8718083e5f5e79698dffb82c415e315bb0cc8a63
astridayala <astriayala06@gmail.com>
2025-09-16 13:39:08 -0600

    Cambios la división de roles y en el Router

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,11 @@
+import 'package:go_router/go_router.dart';
+
+final GoRouter appRouter = GoRouter(
+  initialLocation: '/splash',
+  routes: [
+    GoRoute(
+      path: '/login',
+      name: 
+    )
+  ]
+);
