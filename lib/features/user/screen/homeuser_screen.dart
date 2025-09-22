2e3b50e2df09d0fe29c02c819d93f4b9f0c5a115
ValeriaCastro04 <180575811+ValeriaCastro04@users.noreply.github.com>
2025-09-21 20:41:06 -0600

    Update homeuser_screen.dart

--- lib/features/user/screen/homeuser_screen.dart
+++ lib/features/user/screen/homeuser_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:proyecto_hidoc/common/shared_widgets/footer.dart';
 import 'package:proyecto_hidoc/features/user/widgets/footer_user.dart';
+import 'package:proyecto_hidoc/common/shared_widgets/footer.dart';
 
 class HomeUserScreen extends StatelessWidget {
   static const String name = 'HomeUser';
@@ -8,9 +8,8 @@ class HomeUserScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      appBar: AppBar(title: const Text('HiDoc — Paciente')),
-      body: const Center(child: Text('Home de Paciente')),
       bottomNavigationBar: Footer(
         buttons: userFooterButtons(context, current: UserTab.home),
       ),
