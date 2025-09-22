f1e33313c45ad5a0c76a2c313496b71e0198d7e1
ValeriaCastro04 <180575811+ValeriaCastro04@users.noreply.github.com>
2025-09-21 22:58:51 -0600

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -11,10 +11,6 @@ subprojects {
     project.buildDir = File("${rootProject.buildDir}/${project.name}")
 }
 
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }
